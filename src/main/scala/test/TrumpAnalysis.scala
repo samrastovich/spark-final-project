6c636ce0decbfa6c79cb6e1caef56defabd00136
ngatehouse <nicholasgatehouse@gmail.com>
2018-03-18 17:14:11 -0700

    trump negative positive and neutral analysis

--- src/main/scala/test/TrumpAnalysis.scala
+++ src/main/scala/test/TrumpAnalysis.scala
@@ -1,6 +1,6 @@
 package test
 import java.io.File
-
+import scala.io.Source
 import com.github.tototoshi.csv._
 import org.apache.log4j.{Level, Logger}
 import org.apache.spark.rdd.RDD
@@ -17,34 +17,48 @@ object TrumpAnalysis {
 
     println("Trump Semantic Analysis")
 
-    val TrumpTweets = "datasets/trump/trump_tweets.csv"
-    val positiveWords = sc.textFile("datasets/opinion-words/positive.txt")
-    val negativeWords = sc.textFile("datasets/opinion-words/negative.txt")
-    val reader = CSVReader.open(new File(TrumpTweets))
+    val positiveWords = Source.fromFile("datasets/opinion-words/positive.txt").getLines().toList
+    val negativeWords = Source.fromFile("datasets/opinion-words/negative.txt").getLines().toList
+    val reader = CSVReader.open(new File("datasets/trump/trump_tweets.csv"))
 
     case class ttweet(text : String, date : String, favorites : String , retweets : String, tweetID : String)
     case class tweetOpinion(tweetId : String, favorites : String , retweets : String, positive : Long, negative : Long)
 
-    val tweets = reader.allWithHeaders()
+    val tweets = sc.parallelize(reader.allWithHeaders()
       .map(x=>ttweet(x.getOrElse("Text", "NULL"),
         x.getOrElse("Date", "NULL"),
         x.getOrElse("Favorites", "NULL"),
         x.getOrElse("Retweets", "NULL"),
-        x.getOrElse("Tweet ID", "NULL")))
-
+        x.getOrElse("Tweet ID", "NULL"))))
+    
     tweets.map(x => {
-      val twee = sc.parallelize(x.text.split(" ")).map(x => x.toLowerCase())
-      val negWords = negativeWords.map(x => x.toLowerCase)
-      val posWords = positiveWords.map(x => x.toLowerCase)
-      tweetOpinion(x.tweetID, x.favorites, x.retweets, findNegative(twee , negWords), findPositive(twee, posWords))})
-    }.foreach(println)
-
-  def findNegative(tweet : RDD[String], negativeWords : RDD[String]) : Long = {
-    tweet.intersection(negativeWords).count()
+      val twee = x.text.split(" ").map(x => x.toLowerCase()).toList
+      val posNeg = findNegAndPos(twee, negativeWords, positiveWords)
+      tweetOpinion(x.tweetID, x.favorites, x.retweets, posNeg._1, posNeg._2)})
+    .map(x => {
+      if(x.positive > x.negative)
+        (1, (x.favorites.toInt, x.retweets.toInt))
+      else if (x.positive < x.negative)
+        (-1, (x.favorites.toInt, x.retweets.toInt))
+      else
+        (0, (x.favorites.toInt, x.retweets.toInt))})
+      .aggregateByKey((0, (0.0, 0.0)))(
+      (counta, vals) => (counta._1 + 1, (counta._2._1 + vals._1, counta._2._2 + vals._2 )),
+      (accum1, accum2) => (accum1._1 + accum2._1, (accum1._2._1 + accum2._2._1, accum1._2._2 + accum2._2._2 )))
+      .map(x => (x._1, x._2._2._1/x._2._1, x._2._2._1, x._2._2._2/x._2._1, x._2._2._2,  x._2._1))
+      .foreach(x => {
+        val afav = BigDecimal(x._2).setScale(2, BigDecimal.RoundingMode.HALF_UP).toDouble
+        val aret = BigDecimal(x._4).setScale(2, BigDecimal.RoundingMode.HALF_UP).toDouble
+        if(x._1 < 0)
+          println("Total Negative Tweets: "+ x._6 + "\n[Average Favorites: " + afav + " Total Favorites: " + x._3 + " Average Retweets: " + aret + " Total Retweets: " + x._5+ "]\n")
+        else if (x._1 > 0)
+          println("Total Positive Tweets: "+ x._6 + "\n[Average Favorites: " + afav + " Total Favorites: " + x._3 + " Average Retweets: " + aret + " Total Retweets: " + x._5+ "]\n")
+        else
+          println("Total Neutral Tweets: "+ x._6 + "\n[Average Favorites: " + afav + " Total Favorites: " + x._3 + " Average Retweets: " + aret + " Total Retweets: " + x._5 + "]\n")
+      })
   }
-  def findPositive(tweet : RDD[String], positiveWords : RDD[String]) : Long = {
-    tweet.intersection(positiveWords).count()
+
+  def findNegAndPos(tweet : List[String], negativeWords : List[String], positiveWords : List[String]) : (Int,Int) = {
+    (tweet.intersect(negativeWords).length,tweet.intersect(positiveWords).length)
   }
 }
-
-
